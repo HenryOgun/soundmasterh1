cd7b266fff925843807175bc9a132f74bb70d5e0
Henry Ogun <henryoogun@gmail.com>
2026-05-24 23:56:23 +0100

    Set custom domain www.henryogun.com and fix base path

--- code/henryogun-professional-blog2/vite.config.js
+++ code/henryogun-professional-blog2/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/soundmasterh1/',
+  base: '/',
   server: {
     port: 3002,
     open: true,
